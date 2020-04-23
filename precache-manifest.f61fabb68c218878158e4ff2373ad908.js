fd60efa7d008ad13cf883cd3c9ecc965d7ec45df
ritikvashistha <vashistharitik9@gmail.com>
2020-04-23 21:06:39 +0530

    Updates

---
+++ precache-manifest.f61fabb68c218878158e4ff2373ad908.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26ba19fcce1894256cb859da74646b9",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "f3c191358d145da1ba4d",
+    "url": "/covid/static/css/main.b4790417.chunk.css"
+  },
+  {
+    "revision": "cbd7ab27011135be87ce",
+    "url": "/covid/static/js/2.bdbb11a3.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid/static/js/2.bdbb11a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3c191358d145da1ba4d",
+    "url": "/covid/static/js/main.f1856eed.chunk.js"
+  },
+  {
+    "revision": "6ba7493531c69f6e305f",
+    "url": "/covid/static/js/runtime-main.461c5ddb.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid/static/media/image.d7265326.png"
+  }
+]);
